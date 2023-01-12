3d5dc678709af371a184908ca83ffcca5596668d
Maksim Zakharov <yamyamyami97@gmail.com>
2023-01-12 19:29:59 +0300

    designe fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,11 +14,11 @@ const Header: React.FC = () => {
                 <header className={styles.header}>
                         <div className={styles.content_wrapper}>
                                 <nav className={styles.navigation}>
-                                               {!toggle ? <Link to='/'>Cupaste</Link> : <Link style={{opacity:'0'}} to='/'>Cupaste</Link>} 
-                                        <p onClick={() => setToggle(!toggle)} className={styles.btn_menu}>menu</p>
+                                        {!toggle ? <Link to='/'>Cupaste</Link> : <Link style={{ opacity: '0' }} to='/'>Cupaste</Link>}
                                         <div className={styles.icon_wrap} onClick={() => setToggle(!toggle)}>
                                                 <BurgerToClose thumb={toggle} />
                                         </div>
+                                        
                                         <Modal thumb={toggle} change={() => setToggle(false)} />
                                 </nav>
                         </div>
